1d6216bf0d72ca6f809a792a113ac66e8da5c8d8
Igor Farias <ifarias@savantlabs.io>
2024-11-03 01:28:33 -0300

    plusCode conversor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: '#00579d',
+        'blue-light': '#0068bd',
+        orange: '#EF8300',
+        green: '#7EB61C',
+        yellow: '#FFD600',
+        red: '#D60000',
+        black: '#000000',
+        white: '#FFFFFF',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'Arial', 'sans-serif'],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
